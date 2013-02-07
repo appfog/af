58ec0ee9f4e9cece2310731732a58fe80abd1f38
Tim Santeford <tsantef@gmail.com>
2013-02-07 10:21:02 -0800

    Patched push to support legacy api proxy

--- lib/vmc/cli/app/push.rb
+++ lib/vmc/cli/app/push.rb
@@ -3,6 +3,30 @@ require "vmc/cli/app/push/sync"
 require "vmc/cli/app/push/create"
 require "vmc/cli/app/push/interactions"
 
+# Patched to support legacy api proxy
+module CFoundry::V1
+  class Base < CFoundry::BaseClient
+    def upload_app(name, zipfile, resources = [])
+      payload = {
+        :_method => "put",
+        :application =>
+          UploadIO.new(
+            if zipfile.is_a? File
+              zipfile
+            elsif zipfile.is_a? String
+              File.new(zipfile, "rb")
+            end,
+            "application/zip"),
+        :resources => MultiJson.dump(resources)
+      }
+      # Accept type overrided for compatibility with old api proxy, should just be '*/*'
+      post("apps", name, "application", :payload => payload, :multipart => true, :accept => '*/*; q=0.5, application/xml')
+    rescue EOFError
+      retry
+    end
+  end
+end
+
 module VMC::App
   class Push < Base
     include Sync
